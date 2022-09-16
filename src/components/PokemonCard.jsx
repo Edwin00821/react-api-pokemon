b70f6c1ab5ac56d3f0419491dfd4e3af57e609f8
Edwin00821 <eastudillop2000@alumno.ipn.mx>
2022-09-16 16:42:50 -0500

    Se quita la variable color y se utilizan los metodos correspondientes para obtener los colores, se cambia la semantica del html

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import getPokemonColor from "../js/getPokemonColor";
+import { getPokemonBgColor, getPokemonTxtColor } from "./../js/getPokemonColor";
 import PokemonElement from "./PokemonElement";
 
 export default function PokemonCard({
@@ -12,46 +11,51 @@ export default function PokemonCard({
   types,
   weight,
 }) {
-    let color = getPokemonColor(types[0].type.name)
-    
   return (
-    <div className="bg-white shadow-2xl p-3 rounded-2xl ">
-            <div className={`bg-${color} rounded-2xl pb-2`}>
-                <div className="text-2xl font-bold text-center flex p-5 justify-between  ">
-                <h2 className="text-white uppercase">{ name }</h2>
-                <p >#{ id }</p>
-                </div>
-                <div className="flex justify-center">
-                    <img src={img} alt={name} className=' -mt-5 -mb-20 ' />
-                </div>
-                <div className="bg-white mx-2 rounded-2xl p-5 pt-20">
-                    
-                        <PokemonElement types={ types }/>
-                    
-                    <div className="">
-                        <h3 className={`bg-${color} font-bold text-center m-3 text-xl`}>About</h3>
-                        <div className="grid grid-cols-3 text-black">
-                            <div className="">
-                            <p className="">0.{ weight } kg</p>
-                            <p className="">Weight</p>
-                            </div>
-                            <div className="">
-                            <p className="">0.{ height } m</p>
-                            <p className="">Height</p>
-                            </div>
-                            <div className="">
-                            <p className="first-letter:uppercase">{ abilities }</p>
-                            <p className="">Abilitie</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="">
-                        <h3 className={`text-${color} font-bold text-center m-3 text-xl`}>Base Stats</h3>
-                        <div className="grid grid-cols-3"> 
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div className="h-full w-72 bg-white shadow-2xl p-3 rounded-2xl">
+      <section
+        className={`${getPokemonBgColor(types[0].type.name)} rounded-2xl pb-2`}
+      >
+        <div className="text-2xl font-bold text-center flex p-5 justify-between">
+          <h2 className="text-white uppercase">{name}</h2>
+          <p>#{id}</p>
         </div>
+        <div className="flex justify-center">
+          <img src={img} alt={name} className="-mt-5 -mb-20" />
+        </div>
+        <div className="bg-white mx-2 rounded-2xl p-5 pt-20">
+          <PokemonElement types={types} />
+          <div>
+            <h3
+              className={`${getPokemonBgColor(
+                types[0].type.name
+              )} font-bold text-center m-3 text-xl`}
+            >
+              About
+            </h3>
+            <section className="grid grid-cols-3 text-black">
+              <div>
+                <h3 className="font-semibold">Weight</h3>
+                <p>0.{weight} kg</p>
+              </div>
+              <div>
+                <h3 className="font-semibold">Height</h3>
+                <p>0.{height} m</p>
+              </div>
+              <div>
+                <h3 className="font-semibold">Abilitie</h3>
+                <p className="first-letter:uppercase">{abilities}</p>
+              </div>
+            </section>
+          </div>
+          <div>
+            <h3 className={`${getPokemonTxtColor(types[0].type.name)} font-bold text-center m-3 text-xl`}>
+                Base Stats
+            </h3>
+            <section className="grid grid-cols-3"></section>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
