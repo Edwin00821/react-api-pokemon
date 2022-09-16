e872779528efee8a4948f68a98d60df6916d49ea
Edwin00821 <eastudillop2000@alumno.ipn.mx>
2022-09-16 16:35:14 -0500

    Se utilizan las variables declaradas

--- src/services/getPokemonData.js
+++ src/services/getPokemonData.js
@@ -1,47 +1,32 @@
+import { apiURL, OPTIONS } from '../js/variables.js'
 
-export default async function getPokemonData () {
-    const apiUrl = `https://pokeapi.co/api/v2/pokemon?limit=151`;
-    return fetch(apiUrl, {
-            method: "GET",
-            headers: {
-            "Content-Type": "application/json",
-            },
-        })
-        .then((res) => res.json())
-        .then((response) => {
-          const { results = [] } = response;
-          if (Array.isArray(results)) {
-            //console.log('results', results);
-
-            const POKES = Promise.all(
-                results.map(async ({ url }) => await getPokemonDataById(url))
-            )
-            return POKES;
-            }
-        })
-        .catch((error) => console.log(error));
+export async function getPokemonData() {
+  return fetch(apiURL, OPTIONS)
+    .then(res => res.json())
+    .then(response => {
+      const { results = [] } = response
+      if (Array.isArray(results)) {
+        return Promise.all(
+          results.map(async ({ url }) => await getPokemonDataById(url))
+        )
+      }
+    }) 
+    .catch(error => console.error(error))
 }
 
-async function getPokemonDataById (url) {
-    return fetch(url, {
-            method: "GET",
-            headers: {
-            "Content-Type": "application/json",
-            },
-        })
-        .then((res) => res.json())
-        .then((response) => {
-
-            return {
-                abilities: response.abilities[0].ability.name, 
-                height: response.height, 
-                id: response.id, 
-                name: response.name, 
-                img: response.sprites.other['official-artwork'].front_default, 
-                stats: response.stats, 
-                types: response.types, 
-                weight: response.weight}
-             
-            });
-            
+async function getPokemonDataById(url) {
+  return fetch(url, OPTIONS)
+    .then(res => res.json())
+    .then(response => {
+      return {
+        abilities: response.abilities[0].ability.name,
+        height: response.height,
+        id: response.id,
+        name: response.name,
+        img: response.sprites.other['official-artwork'].front_default,
+        stats: response.stats,
+        types: response.types,
+        weight: response.weight,
+      }
+    })
 }
