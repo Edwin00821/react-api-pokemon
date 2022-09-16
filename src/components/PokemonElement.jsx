6146bfd5dddde1b728bdac7e840c235276a8b2fc
Edwin00821 <eastudillop2000@alumno.ipn.mx>
2022-09-16 16:52:49 -0500

    Se agrega la expresion regular para aplicarla en la key y el import

--- src/components/PokemonElement.jsx
+++ src/components/PokemonElement.jsx
@@ -1,14 +1,23 @@
-import getPokemonColor from "../js/getPokemonColor";
+import { getPokemonBgColor } from "./../js/getPokemonColor";
 
-export default function PokemonElement({types}) {
-    
-    return (
-        <div className="flex justify-center">
-        {
-            types.map(({type}) => {
-                return <p key={type.url} className={`bg-${getPokemonColor(type.name)} text-white m-1 p-1 px-3 rounded-2xl font-bold mx-3 first-letter:uppercase`}>{type.name}</p>
-            })
-        }
-        </div>
-    );
+export default function PokemonElement({ types }) {
+  return (
+    <div className="flex justify-center">
+      {types.map((type) => {
+        const { name, url } = type.type;
+        // console.log(name)
+        const regex = /https:\/\/pokeapi.co\/api\/v2\/type\/|\//g;
+        const key = url.replace(regex, "");
+        // const stringName = name.toString()
+        // console.log(stringName)
+        return (
+          <p
+            key={key}
+            className={`${getPokemonBgColor(name)} text-white m-1 p-1 px-3 rounded-2xl font-bold mx-3 first-letter:uppercase`}>
+            {name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
