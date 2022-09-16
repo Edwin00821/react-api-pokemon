b3ec8f4b33ead9f33f2b00cffadc94146ac76016
Edwin00821 <eastudillop2000@alumno.ipn.mx>
2022-09-15 19:46:25 -0500

    Subiendo el proyecto

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import getPokemonData from "./services/getPokemonData";
+import ListOfPokemons from "./components/ListOfPokemons";
+
+function App() {
+  const [pokemons, setPokemoms] = useState([]);
+
+  useEffect(() => {
+    getPokemonData().then((pokemons) => setPokemoms(pokemons));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl">Pokedex</h1>
+      <h2>Astudillo Perez Edwin Uriel</h2>
+      <ListOfPokemons pokemons={pokemons} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListOfPokemons.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PokemonCard from "./PokemonCard";
+
+export default function ListOfPokemons({ pokemons }) {
+  return (
+    <div className="container grid grid-cols-1 sm:grid-cols-3 gap-6 my-10 justify-items-center m-auto w-3/5">
+      {
+      pokemons.map(({abilities, height, id, name, img, stats, types, weight}) => {
+        return <PokemonCard 
+        abilities={abilities}
+        height={height}
+        id={id}
+        name={name}
+        img={img}
+        stats={stats}
+        types={types}
+        weight={weight}
+        />
+      })
+      }
+    </div>
+  );
+}
